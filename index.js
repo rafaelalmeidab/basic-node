80a68fd2749864832a55317d53305febc540d4cd
r.almeida <rafael.almeida@rbmweb.com.br>
2023-06-16 13:43:59 -0300

    feature: testing new route

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+// const express = require('express');
 import express from 'express';
 const app = express();
 const port = 3000;
 
-// Rotas do seu projeto
-app.get('/', (req, res) => {
-  res.send('Olá, mundo!');
-});
-
 // Inicia o servidor
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Running server on http://localhost:${port}`);
 });
+
+// Rotas do seu projeto
+app.get('/', (req, res) => {
+  res.send('Hi!');
+});
+
+app.get('/timestamp', (req, res) => {
+  var timestamp = new Date();
+  res.send(timestamp);
+});
+
+app.get('/name', (req, res) => {
+  res.send("rafael");
+});
+
