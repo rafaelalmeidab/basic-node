c1da1e9698798465ac77ea1cfc71e8adc7d0297e
r.almeida <rafael.almeida@rbmweb.com.br>
2023-06-19 13:19:45 -0300

    updating routes

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const port = 3000;
 // Waiting:A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
 // Waiting:A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
 // Waiting:Your project can handle dates that can be successfully parsed by new Date(date_string)
-// Waiting:If the input date string is invalid, the API returns an object having the structure { error : "Invalid Date" }
-// Waiting:An empty date parameter should return the current time in a JSON object with a unix key
-// Waiting:An empty date parameter should return the current time in a JSON object with a utc key
+// Waiting:If the input date string is invalid, the API returns an object having the structure { error : "Invalid Date" } - OK!
+// Waiting:An empty date parameter should return the current time in a JSON object with a unix key - OK!
+// Waiting:An empty date parameter should return the current time in a JSON object with a utc key - OK!
 
 app.listen(port, () => {
   console.log(`Running server on http://localhost:${port}`);
@@ -18,15 +18,33 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
   const utcTimestamp = new Date().getTime();
-  // console.log(utcTimestamp);
-  var arr = [];
-  arr['unix'] = utcTimestamp;
-
   const gmtDateTime = new Date().toUTCString();
-  // console.log(gmtDateTime);
-  arr['utc'] = gmtDateTime;
 
+  const obj = { unix: utcTimestamp, utc: gmtDateTime};
+  const jsonObj = JSON.stringify(obj);
 
-  // var getTime = new Date().getTime();
-  res.send(json_stringify(arr));
+  res.send(jsonObj);
+});
+
+app.get('/api/:date?', (req, res) => {
+
+  var dateParam = req.url.split('/').pop();
+  console.log(dateParam);
+  var obj = '';
+  const currentDate = new Date(dateParam);
+  console.log(currentDate instanceof Date);
+  
+  if(currentDate.toString() == 'Invalid Date'){
+    obj = { error: currentDate.toString()};
+    dateParam = JSON.stringify(obj);
+  }
+  else{
+    const utcTimestamp = currentDate.getTime();
+    const gmtDateTime  = currentDate.toUTCString();
+  
+    obj = { unix: utcTimestamp, utc: gmtDateTime};
+    dateParam = JSON.stringify(obj);
+  }
+  
+  res.send(dateParam);
 });
